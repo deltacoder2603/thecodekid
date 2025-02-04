e9d6bd9f22874cc08854ebe64068a965d87514a3
Srikant Pandey <154867795+deltacoder2603@users.noreply.github.com>
2025-02-04 12:24:49 +0530

    Update page.tsx

--- app/schools/page.tsx
+++ app/schools/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import { Star, MonitorSmartphone, BookOpen, Gamepad, ChevronLeft, ChevronRight, ArrowRight, Quote } from 'lucide-react';
 import { motion } from 'framer-motion';
