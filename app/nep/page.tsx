8e4b8d931c0421355a53a210cb320761997b2892
Srikant Pandey <154867795+deltacoder2603@users.noreply.github.com>
2025-02-04 16:08:54 +0530

    Update page.tsx

--- app/nep/page.tsx
+++ app/nep/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, BookOpen, GraduationCap, Brain } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card"
 import Footer from "@/components/Footer";
